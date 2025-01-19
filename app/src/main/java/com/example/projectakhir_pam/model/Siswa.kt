81464dd33e4db82b19d4c494b523ab5851cfe03d
ulfhiraa <studyiraa@gmail.com>
2025-01-19 21:18:47 +0700

    ADD - model Siswa

---
+++ app/src/main/java/com/example/projectakhir_pam/model/Siswa.kt
@@ -0,0 +1,33 @@
+package com.example.projectakhir_pam.model
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Siswa(
+//    @SerialName("id_siswa")
+    val id_siswa: String,
+
+    @SerialName("nama_siswa")
+    val namaSiswa: String,
+    val email: String,
+
+    @SerialName("nomor_telepon")
+    val noTelpSiswa: String
+)
+
+
+// ADD - response JSON
+@Serializable
+data class SiswaResponse(
+    val status: Boolean,
+    val message: String,
+    val data: List<Siswa>
+)
+
+@Serializable
+data class SiswaResponseDetail(
+    val status: Boolean,
+    val message: String,
+    val data: Siswa
+)
