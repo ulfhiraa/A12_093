ff694617ece3a668902af2e6866c07fa1916a953
ulfhiraa <studyiraa@gmail.com>
2025-01-19 22:02:39 +0700

    ADD - DestinasiNavigasi untuk mendefinisikan route dan titleRes dalam navigasi

---
+++ app/src/main/java/com/example/projectakhir_pam/ui/navigasi/DestinasiNavigasi.kt
@@ -0,0 +1,7 @@
+package com.example.projectakhir_pam.ui.navigasi
+
+// untuk mendefinisikan route dan titleRes dalam navigasi
+interface DestinasiNavigasi {
+    val route: String
+    val titleRes: String
+}
