613115b49f39bc3b3e8a7627678f2f113360e994
ulfhiraa <studyiraa@gmail.com>
2025-01-20 19:20:46 +0700

    ADD - UpdateView.kt untuk menampilkan pembaruan data

---
+++ app/src/main/java/com/example/projectakhir_pam/ui/view/Siswa/UpdateView.kt
@@ -0,0 +1,56 @@
+package com.example.projectakhir_pam.ui.view.Siswa
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.projectakhir_pam.ui.customwidget.CustomeTopAppBar
+import com.example.projectakhir_pam.ui.navigasi.DestinasiNavigasi
+import com.example.projectakhir_pam.ui.viewmodel.PenyediaViewModel
+import com.example.projectakhir_pam.ui.viewmodel.Siswa.UpdateViewModel
+import kotlinx.coroutines.launch
+
+// update view : untuk menampiilkan update data
+
+object DestinasiUpdate: DestinasiNavigasi {
+    override val route = "update"
+    override val titleRes = "Update Siswa" // judul halaman
+    const val id_siswa = "id_siswa"
+    val routeWithArgs = "$route/{$id_siswa}"
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun UpdateView( // untuk memperbarui data mahasiswa dengan navigasi kembali
+    navigateBack: () -> Unit,
+    onNavigateUp: () -> Unit,
+    modifier: Modifier = Modifier,
+    updateViewModel: UpdateViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
+    val coroutineScope = rememberCoroutineScope()
+    Scaffold(
+        topBar = {
+            CustomeTopAppBar(
+                title = "Update Mahasiswa",
+                canNavigateBack = true,
+                navigateUp = navigateBack
+            )
+        },
+        modifier = modifier
+    ) { innerPadding ->
+        EntryBody(
+            insertUiState = updateViewModel.updateUiState,
+            onMhsValueChange = updateViewModel::updateInsertMhsState,
+            onSaveClick = {
+                coroutineScope.launch {
+                    updateViewModel.updateMhs()
+                    onNavigateUp()
+                }
+            },
+            modifier = Modifier.padding(innerPadding)
+        )
+    }
+}
