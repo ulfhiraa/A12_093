5154021768c088b9e7b91a7223e7f26a8a383e41
ulfhiraa <studyiraa@gmail.com>
2025-01-20 17:07:06 +0700

    ADD - SiswaContainer.kt untuk mengelola koneksi API siswa dan menyediakan akses ke data siswa lewat siswaRepository

---
+++ app/src/main/java/com/example/projectakhir_pam/dependenciesinjection/SiswaContainer.kt
@@ -0,0 +1,39 @@
+package com.example.projectakhir_pam.dependenciesinjection
+
+import com.example.projectakhir_pam.repository.NetworkSiswaRepository
+import com.example.projectakhir_pam.repository.SiswaRepository
+import com.example.projectakhir_pam.service.SiswaService
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import retrofit2.Retrofit
+
+// untuk mendeklarasikan siswaRepository
+interface SisContainer{
+    val siswaRepository: SiswaRepository
+}
+
+// untuk menyediakan
+class SiswaContainer : SisContainer{
+    private val baseUrl = "http://10.0.2.2:3000/api/siswa/" // untuk mengakses API
+
+    private val json = Json { ignoreUnknownKeys = true } //  mengatur format data JSON.
+
+    //menggunakan Retrofit (sebuah pustaka untuk memudahkan komunikasi dengan API)
+    // untuk membuat repositori yang mengelola data siswa dari API.
+
+    private val retrofit: Retrofit = Retrofit.Builder()
+        .addConverterFactory(json.asConverterFactory("application/json".toMediaType()))
+        .baseUrl(baseUrl)
+        .build()
+
+    // Membuat layanan untuk mengakses data siswa melalui API
+    // inisialisasi SiswaService untuk mengakses API siswa.
+    private val siswaService: SiswaService by lazy {
+        retrofit.create(SiswaService::class.java)
+    }
+
+    override val siswaRepository: SiswaRepository by lazy {
+        NetworkSiswaRepository(siswaService)
+    }
+}
