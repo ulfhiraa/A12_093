1f5c043f872e2f6ca2ce99749a7877b06865d4d6
ulfhiraa <studyiraa@gmail.com>
2025-01-20 19:25:03 +0700

    ADD - fungsi SiswaApp untuk menangani scroll behavior dan menampilkan PengelolaHalaman, menggunakan surface untuk pembungkus tampilan dengan ukuran penuh

---
+++ app/src/main/java/com/example/projectakhir_pam/ui/viewmodel/Siswa/SiswaApp.kt
@@ -0,0 +1,34 @@
+package com.example.projectakhir_pam.ui.viewmodel.Siswa
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import com.example.projectakhir_pam.ui.navigasi.PengelolaHalaman
+
+// untuk menangani scroll behavior dan menampilkan PengelolaHalaman sebagai konten utama,
+// menggunakan surface untuk pembungkus tampilan dengan ukuran penuh
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SiswaApp() {
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+    Scaffold(
+        modifier = Modifier.
+        nestedScroll(scrollBehavior.nestedScrollConnection),
+        // topBar = { TopAppBar(scrollBehavior = scrollBehavior) }
+    ) {
+        Surface(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(it)
+        ) {
+            PengelolaHalaman()
+        }
+    }
+}
