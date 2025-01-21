b35a0be35838cb4d1cf85ba63b5156f1d3306530
ulfhiraa <studyiraa@gmail.com>
2025-01-21 22:26:01 +0700

    ADD - HomeInstViewModel untuk mengatur status UI, mengambil, menghapus, dan mengedit data

---
+++ app/src/main/java/com/example/projectakhir_pam/ui/viewmodel/Instruktur/HomeInstViewModel.kt
@@ -0,0 +1,56 @@
+package com.example.projectakhir_pam.ui.viewmodel.Instruktur
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import coil.network.HttpException
+import com.example.projectakhir_pam.model.Instruktur
+import com.example.projectakhir_pam.repository.InstrukturRepository
+import com.example.projectakhir_pam.ui.viewmodel.Siswa.HomeSisUiState
+import kotlinx.coroutines.launch
+import java.io.IOException
+
+// HomeViewModel : mengelola data dan status tampilan (UI) terkait data siswa
+
+sealed class HomeInstUiState {
+    data class Success(val instruktur: List<Instruktur>) : HomeInstUiState()
+    object Error : HomeInstUiState()
+    object Loading : HomeInstUiState()
+}
+
+class HomeInstViewModel(private val inst: InstrukturRepository) : ViewModel() {
+    var instUIState: HomeInstUiState by mutableStateOf(HomeInstUiState.Loading)
+        private set
+
+    init {
+        getInst() // untuk mengambil daftar instruktur dari repository (InstrukturRepository)
+    }
+
+    fun getInst() {
+        viewModelScope.launch {
+            instUIState = HomeInstUiState.Loading
+            instUIState = try {
+                HomeInstUiState.Success(inst.getInstruktur().data)
+            } catch (e: IOException) {
+                HomeInstUiState.Error
+            } catch (e: HttpException) {
+                HomeInstUiState.Error
+            }
+        }
+    }
+
+    //untuk menghapus data instruktur berdasarkan ID yang diberikan
+    fun deleteInst(id_instruktur: String) {
+        viewModelScope.launch {
+            try {
+                inst.deleteInstruktur(id_instruktur)
+            } catch (e: IOException){
+                HomeSisUiState.Error
+            } catch (e: HttpException){
+                HomeSisUiState.Error
+            }
+        }
+    }
+}
