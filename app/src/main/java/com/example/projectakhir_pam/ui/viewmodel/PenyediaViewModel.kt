d3caf2c600245adcd8a664f1a44b55c79dc3c07c
ulfhiraa <studyiraa@gmail.com>
2025-01-22 01:13:55 +0700

    ADD - inisialisasi view model instruktur home, insert, update, dan delete di penyedia view model

--- app/src/main/java/com/example/projectakhir_pam/ui/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/projectakhir_pam/ui/viewmodel/PenyediaViewModel.kt
@@ -6,7 +6,10 @@ import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.projectakhir_pam.BimbelApplications
+import com.example.projectakhir_pam.ui.viewmodel.Instruktur.DetailInstViewModel
 import com.example.projectakhir_pam.ui.viewmodel.Instruktur.HomeInstViewModel
+import com.example.projectakhir_pam.ui.viewmodel.Instruktur.InsertInstViewModel
+import com.example.projectakhir_pam.ui.viewmodel.Instruktur.UpdateInstViewModel
 import com.example.projectakhir_pam.ui.viewmodel.Siswa.DetailSisViewModel
 import com.example.projectakhir_pam.ui.viewmodel.Siswa.HomeSisViewModel
 import com.example.projectakhir_pam.ui.viewmodel.Siswa.InsertSisViewModel
@@ -63,8 +66,36 @@ object PenyediaViewModel{
                     .instrukturRepository
             )
         }
-    }
 
+        // INSERT INSTRUKTUR
+        initializer {
+            InsertInstViewModel(
+                BimbelApplications()
+                    .bimbelContainer
+                    .instrukturRepository
+            )
+        }
+
+        // DETAIL INSTRUKTUR
+        initializer {
+            DetailInstViewModel(
+                createSavedStateHandle(),
+                BimbelApplications()
+                    .bimbelContainer
+                    .instrukturRepository
+            )
+        }
+
+        // UPDATE INSTRUKTUR
+        initializer {
+            UpdateInstViewModel(
+                createSavedStateHandle(),
+                BimbelApplications()
+                    .bimbelContainer
+                    .instrukturRepository
+            )
+        }
+    }
     fun CreationExtras.BimbelApplications(): BimbelApplications =
         (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as BimbelApplications)
 }
