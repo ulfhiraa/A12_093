c66090a261eb80f2576605643f72225c443b7452
ulfhiraa <studyiraa@gmail.com>
2025-01-22 15:06:14 +0700

    ADD - Kursus model untuk mendefinisikan informasi entitas dan atribut, serta menangani respon API

---
+++ app/src/main/java/com/example/projectakhir_pam/model/Kursus.kt
@@ -0,0 +1,33 @@
+package com.example.projectakhir_pam.model
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+// Kursus data model dan response classes untuk menangani respon API
+// mendefinisikan informasi entitas dan atribut
+
+@Serializable
+data class Kursus(
+    val id_kursus: String,
+
+    @SerialName("nama_kursus")
+    val namaKursus: String,
+    val deskripsi: String,
+    val harga: String,
+    val id_instruktur: String
+)
+
+// ADD - response JSON
+@Serializable
+data class KursusResponse(
+    val status: Boolean,
+    val message: String,
+    val data: List<Kursus>
+)
+
+@Serializable
+data class KursusResponseDetail(
+    val status: Boolean,
+    val message: String,
+    val data: Kursus
+)
