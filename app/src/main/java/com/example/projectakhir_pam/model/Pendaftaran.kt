6df3ebc2e45af918be2a21acd2b6e12ab9fbeecb
ulfhiraa <studyiraa@gmail.com>
2025-01-24 02:11:26 +0700

    ADD - model pendaftaran untuk mendefinisikan entitas atribut dan menangani respon API

---
+++ app/src/main/java/com/example/projectakhir_pam/model/Pendaftaran.kt
@@ -0,0 +1,33 @@
+package com.example.projectakhir_pam.model
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+// pendaftaran data model dan response classes untuk menangani respon API
+// mendefinisikan informasi entitas dan atribut
+
+@Serializable
+data class Pendaftaran(
+    val id_pendaftaran: String,
+    val id_siswa: String,
+    val id_kursus: String,
+
+    @SerialName("tanggal_pendaftaran")
+    val tglDaftar: String,
+    val status: String
+)
+
+// ADD - response JSON
+@Serializable
+data class PendaftaranResponse(
+    val status: Boolean,
+    val message: String,
+    val data: List<Pendaftaran>
+)
+
+@Serializable
+data class PendaftaranResponseDetail(
+    val status: Boolean,
+    val message: String,
+    val data: Pendaftaran
+)
