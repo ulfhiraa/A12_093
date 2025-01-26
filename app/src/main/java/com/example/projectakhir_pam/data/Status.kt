411c2e895d40f3eff15cc73b72155a6e589a9a20
ulfhiraa <studyiraa@gmail.com>
2025-01-26 11:26:05 +0700

    ADD - Objek Status untuk menyimpan data status pendaftaran aktif atau tidak aktif

---
+++ app/src/main/java/com/example/projectakhir_pam/data/Status.kt
@@ -0,0 +1,9 @@
+package com.example.projectakhir_pam.data
+
+// untuk menyimpan data status pendaftaran
+object Status {
+    val listStatus= listOf(
+        "Aktif",
+        "Tidak AKtif"
+    )
+}
