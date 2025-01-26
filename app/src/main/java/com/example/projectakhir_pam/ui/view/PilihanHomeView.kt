c06e5f0990660b748768fb577ed1d4cdc2a77a0f
ulfhiraa <studyiraa@gmail.com>
2025-01-26 11:06:40 +0700

    UPDATE - Styling halaman utama

--- app/src/main/java/com/example/projectakhir_pam/ui/view/PilihanHomeView.kt
+++ app/src/main/java/com/example/projectakhir_pam/ui/view/PilihanHomeView.kt
@@ -15,7 +15,9 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.List
+import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -28,6 +30,8 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -43,69 +47,53 @@ object DestinasiHome : DestinasiNavigasi {
 
 @Composable
 fun SectionHeader() {
-    Box(modifier = Modifier
-        .fillMaxWidth()
-        .background(color = Color(0xFFd9d9d9),
-            RoundedCornerShape(bottomEnd = 100.dp)
-        )
-    ){
-        Box(){
-            // LOGO
-            Column(modifier = Modifier.fillMaxWidth(),
-                horizontalAlignment = Alignment.End,
-                verticalArrangement = Arrangement.Center,
-
-//                horizontalAlignment = Alignment.Start,
-//                verticalArrangement = Arrangement.SpaceBetween
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(
+                color = Color(0xFFf7f0f6), // Warna pink pastel lembut
+                shape = RoundedCornerShape(bottomEnd = 100.dp)
+            )
+            .padding(bottom = 50.dp)
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Column(
+                modifier = Modifier.padding(start = 16.dp),
+                verticalArrangement = Arrangement.Center
             ) {
-                Spacer(Modifier.padding(25.dp))
-
-                Image(
-                    painter = painterResource(id = R.drawable.logo),
-                    contentDescription = " ",
-                    Modifier
-                        .size(80.dp)
-//                        .clip(RoundedCornerShape(100.dp))
-//                        .shadow(50.dp, RoundedCornerShape(100.dp))
-                )
-            }
-
-            Column (
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(start = 16.dp),
-                horizontalAlignment = Alignment.Start,
-                verticalArrangement = Arrangement.SpaceBetween,
-
-//                horizontalAlignment = Alignment.End,
-//                verticalArrangement = Arrangement.Center,
-            ){
-                Spacer(Modifier.padding(30.dp))
-                Icon(
-                    Icons.Filled.List,
-                    contentDescription = " ",
-                    tint = Color.Black,
-                )
-
-                // Header Title
-                Spacer(Modifier.padding(8.dp))
+                Spacer(modifier = Modifier.height(85.dp))
                 Text(
-                    text = "UTBK ",
-                    style = MaterialTheme.typography.headlineLarge.copy(
-                        fontWeight = FontWeight.W300,
-                        color = Color.Black,
-                    ),
-                    fontSize = 30.sp,
-                )
-                Text(
-                    text = "Course ",
-                    style = MaterialTheme.typography.headlineLarge.copy(
-                        fontWeight = FontWeight.Light,
-                        color = MaterialTheme.colorScheme.onPrimary
+                    text = "UTBK Course",
+                    style = TextStyle(
+                        fontFamily = FontFamily.Serif,
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 40.sp,
+                        color = Color(0xFF333333) // Warna teks lebih gelap
                     )
                 )
-                Spacer(Modifier.padding(16.dp))
+                Text(
+                    text = "Ubur-ubur ikan lele, " +
+                            "\nSukses di PTN impian lee!",
+                    style = TextStyle(
+                        fontFamily = FontFamily.SansSerif,
+                        fontWeight = FontWeight.Light,
+                        fontSize = 25.sp,
+                        color = Color(0xFF7C7C7C) // Warna teks deskripsi lebih terang
+                    )
+                )
             }
+
+            Image(
+                painter = painterResource(id = R.drawable.logo),
+                contentDescription = "Logo",
+                modifier = Modifier
+                    .size(100.dp)
+                    .padding(end = 16.dp)
+            )
         }
     }
 }
@@ -135,20 +123,21 @@ fun PilihanHomeView(
             modifier = Modifier
                 .fillMaxSize()
                 .padding(16.dp),
-            horizontalAlignment = Alignment.CenterHorizontally
+            horizontalAlignment = Alignment.Start
         ) {
             Text(
-                text = "Selamat Datang! >_<",
-                style = MaterialTheme.typography.headlineMedium,
+                text = "안녕 하세요, \n" +
+                        "Selamat Datang! >_<",
+                style = MaterialTheme.typography.headlineSmall,
                 modifier = Modifier.padding(5.dp)
             )
 
-            Text(
-                text = "                 Raih skor memuaskan" +
-                        "\ndan masuk PTN impianmu bersama kami \uD83D\uDCAA",
-                style = MaterialTheme.typography.bodyLarge,
-                modifier = Modifier.padding(5.dp)
-            )
+//            Text(
+//                text = "                 Raih skor memuaskan" +
+//                        "\ndan masuk PTN impianmu bersama kami \uD83D\uDCAA",
+//                style = MaterialTheme.typography.bodyLarge,
+//                modifier = Modifier.padding(5.dp)
+//            )
 
             Spacer(modifier = Modifier.padding(bottom = 40.dp))
 
@@ -160,16 +149,17 @@ fun PilihanHomeView(
                 horizontalArrangement = Arrangement.SpaceEvenly
             ) {
                 // HOME KURSUS CARD
-                OutlinedCard(
+                ElevatedCard(
                     onClick = onKursusClick,
                     modifier = Modifier
                         .weight(1f)
                         .padding(horizontal = 8.dp)
+                        .shadow(12.dp)
                 ) {
                     Column(
                         modifier = Modifier
                             .fillMaxWidth()
-                            .padding(16.dp),
+                            .padding(10.dp),
                         horizontalAlignment = Alignment.CenterHorizontally
                     ) {
                         Spacer(modifier = Modifier.height(20.dp))
@@ -178,14 +168,14 @@ fun PilihanHomeView(
                             painter = painterResource(id = R.drawable.kursus),
                             contentDescription = "Home Kursus Icon",
                             modifier = Modifier
-                                .size(60.dp)
+                                .size(70.dp)
                         )
                         Spacer(modifier = Modifier.height(10.dp))
 
                         // Teks "Home Kursus"
                         Text(
                             text = "Kursus",
-                            style = MaterialTheme.typography.bodyLarge.copy(fontWeight = FontWeight.Bold),
+                            style = MaterialTheme.typography.bodyLarge.copy(fontWeight = FontWeight.ExtraBold),
                             textAlign = TextAlign.Center
                         )
                         Spacer(modifier = Modifier.height(20.dp))
@@ -193,11 +183,12 @@ fun PilihanHomeView(
                 }
 
                 // HOME SISWA CARD
-                OutlinedCard(
+                ElevatedCard(
                     onClick = onSiswaClick,
                     modifier = Modifier
                         .weight(1f)
                         .padding(horizontal = 8.dp)
+                        .shadow(12.dp)
                     ,
                     colors = CardDefaults.outlinedCardColors(
                         // containerColor = Color(0xFFFFCDD2) // Pink pastel color
@@ -206,7 +197,7 @@ fun PilihanHomeView(
                     Column(
                         modifier = Modifier
                             .fillMaxWidth()
-                            .padding(16.dp),
+                            .padding(10.dp),
                         horizontalAlignment = Alignment.CenterHorizontally
                     ) {
                         Spacer(modifier = Modifier.height(20.dp))
@@ -215,7 +206,7 @@ fun PilihanHomeView(
                             painter = painterResource(id = R.drawable.siswa),
                             contentDescription = "Home Siswa Icon",
                             modifier = Modifier
-                                .size(60.dp)
+                                .size(70.dp)
                         )
                         Spacer(modifier = Modifier.height(10.dp))
 
@@ -239,16 +230,17 @@ fun PilihanHomeView(
                 horizontalArrangement = Arrangement.SpaceEvenly
             ) {
                 // HOME INSTRUKTUR CARD
-                OutlinedCard(
+                ElevatedCard(
                     onClick = onInstrukturClick,
                     modifier = Modifier
                         .weight(1f)
                         .padding(horizontal = 8.dp)
+                        .shadow(12.dp)
                 ) {
                     Column(
                         modifier = Modifier
                             .fillMaxWidth()
-                            .padding(16.dp),
+                            .padding(10.dp),
                         horizontalAlignment = Alignment.CenterHorizontally
                     ) {
                         Spacer(modifier = Modifier.height(20.dp))
@@ -257,7 +249,7 @@ fun PilihanHomeView(
                             painter = painterResource(id = R.drawable.pengajar),
                             contentDescription = "Home Instruktur Icon",
                             modifier = Modifier
-                                .size(60.dp)
+                                .size(70.dp)
                         )
                         Spacer(modifier = Modifier.height(10.dp))
 
@@ -272,16 +264,17 @@ fun PilihanHomeView(
                 }
 
                 // HOME PENDAFTARAN CARD
-                OutlinedCard(
+                ElevatedCard(
                     onClick = onPendaftaranClick,
                     modifier = Modifier
                         .weight(1f)
                         .padding(horizontal = 8.dp)
+                        .shadow(12.dp)
                 ) {
                     Column(
                         modifier = Modifier
                             .fillMaxWidth()
-                            .padding(16.dp),
+                            .padding(10.dp),
                         horizontalAlignment = Alignment.CenterHorizontally
                     ) {
                         Spacer(modifier = Modifier.height(20.dp))
@@ -290,7 +283,7 @@ fun PilihanHomeView(
                             painter = painterResource(id = R.drawable.pendaftaran),
                             contentDescription = "Home Pendaftaran Icon",
                             modifier = Modifier
-                                .size(60.dp)
+                                .size(70.dp)
                         )
                         Spacer(modifier = Modifier.height(10.dp))
 
